34511435d189eae4a7a5da78a905c888d2e3fe02
Luana Correia <luacooli@gmail.com>
2024-08-22 23:01:12 -0300

    feat: range chart

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,17 @@
   <h1>Vue Grid graph demo</h1>
 
   <h2>Range Chart</h2>
-  <ag-grid-vue
-    :rowData="rowData"
-    :columnDefs="columnDefs"
-    style="width: 98%; height: 500px"
-    class="ag-theme-quartz"
-    :enableCharts="true"
-    :enableRangeSelection="true"
-    rowGroupPanelShow="always"
-  >
+  <ag-grid-vue :rowData="rowData" :columnDefs="columnDefs" style="width: 98%; height: 500px" class="ag-theme-quartz"
+    :enableCharts="true" :enableRangeSelection="true" :enableRangeHandle="true" @grid-ready="onGridReady"
+    @rangeSelectionChanged="seeChange" rowGroupPanelShow="always">
   </ag-grid-vue>
+
+  <!--
+  enableCharts -> enable charts creation
+  enableRangeSelection -> enable charts selection by mouse
+  enableRangeHandle -> enable resizeble range selection
+  -->
+  <button @click="clearSelection">clear selection</button>
 </template>
 
 <script>
@@ -41,9 +42,34 @@ export default {
       { athlete: "Katie Ledecky", gold: 4, silver: 0, bronze: 0 },
     ]);
 
+    const gridApi = ref(null);
+
+    const onGridReady = (params) => {
+      console.log('it is ready');
+
+      gridApi.value = params.api;
+      console.log(gridApi);
+    };
+
+    const seeChange = (s, f) => {
+      console.log('Range selection changed:', s, f);
+    }
+
+    const clearSelection = () => {
+      console.log(gridApi.value);
+
+      if (gridApi.value) {
+        gridApi.value.clearRangeSelection();
+        console.log("Range selection cleared");
+      }
+    }
+
     return {
       rowData,
       columnDefs,
+      onGridReady,
+      seeChange,
+      clearSelection,
     };
   },
 };
